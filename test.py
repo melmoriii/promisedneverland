6ff66f08880bb10424b4efc6d9221c5a4fc698e3
melmoriii <polyna.myroshnychenko@gmail.com>
2021-07-17 16:57:57 +0300

    fixed tree project; correctly placed authorisation menu*

--- test.py
+++ test.py
@@ -1,8 +1,17 @@
-list1 = [1,2,3]
-element = list1[2]
-print(list1)
-print(element)
-print("vasya22"+2)
-# print(element+2)
-# list1.append(2)
-# print(list1)
+name1 = "line"
+name2 = "longline"
+name3 = "biglongline"
+street = [name1, name2]
+street.append(name3)
+print(street)
+victim = street[-1]
+print(victim)
+street[0] = "newline"
+print(street)
+street[-1] = "verybiglongline"
+print(street)
+contact = ("+380970979665", "name1")
+print(contact[1])
+print(type(street), type(contact))
+print(dir(street))
+print(dir(contact))
