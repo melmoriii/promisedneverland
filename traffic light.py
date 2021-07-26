f9563c69182086a23a829de304f9833c1eb65e1e
melmoriii <polyna.myroshnychenko@gmail.com>
2021-07-26 14:27:16 +0300

    traffic light.py done; на этом мои полномочия всё

--- traffic light.py
+++ traffic light.py
@@ -1,1 +1,11 @@
-color = "red"
+color = input("enter color")
+red = "red"
+yellow = "yellow"
+green = "green"
+if color == red:
+    print("wait for green color")
+else:
+    if color == yellow:
+        print("get ready to go")
+    else:
+        print("you can go")
