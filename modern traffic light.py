f4a805749d30bc21d02429ba7141cac7f9f0c65f
melmoriii <polyna.myroshnychenko@gmail.com>
2021-08-14 17:32:13 +0300

    made working program

--- modern traffic light.py
+++ modern traffic light.py
@@ -26,23 +26,25 @@
 #         print("red")
 #         button = input()
 import time
+
 red_seconds = 10
 green_seconds = 12
 button = False
 while True:
-    if button == False:
+    while not button:
         print("red")
         button = input()
-    elif button == "on":
+    while red_seconds != 0:
+        print(f"seconds:{red_seconds}")
+        time.sleep(0.5)
+        red_seconds -= 1
+    print("green")
+    print("you can go")
+    while green_seconds != 0:
         print(f"seconds:{green_seconds}")
         time.sleep(0.5)
         green_seconds -= 1
-    else:
-        print("green")
-        print("you can go")
-        if green_seconds != 0:
-            print(f"seconds:{red_seconds}")
-            time.sleep(0.5)
-            red_seconds -= 1
-        else:
-            print("stop")
+    print("stop")
+    button = False
+    red_seconds = 10
+    green_seconds = 12
